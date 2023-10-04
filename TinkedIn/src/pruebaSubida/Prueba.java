f4f7a393e58bc7650115fa8cab4b0ec894211f4e
i.eguiluz <i.eguiluz@E106L22.opendeusto.es>
2023-10-04 10:13:33 +0200

    Commit biende importante

--- TinkedIn/src/pruebaSubida/Prueba.java
+++ TinkedIn/src/pruebaSubida/Prueba.java
@@ -5,7 +5,9 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		//Prueba
+		for(;;) {
+			System.out.println("Aitor es un pelado");
+		}
 	}
 
 }
