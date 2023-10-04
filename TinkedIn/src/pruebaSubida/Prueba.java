7c894882e031b25963369c65e22354f135e9e90d
i.eguiluz <i.eguiluz@E106L22.opendeusto.es>
2023-10-04 10:26:10 +0200

    Commit 1

--- TinkedIn/src/pruebaSubida/Prueba.java
+++ TinkedIn/src/pruebaSubida/Prueba.java
@@ -5,8 +5,11 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		int contador = 0;
 		for(;;) {
-			System.out.println("Aitor es un pelado");
+			System.out.println("Aitor que haces con tu vida");
+			contador ++;
+			if(contador == 5) return;	
 		}
 	}
 
