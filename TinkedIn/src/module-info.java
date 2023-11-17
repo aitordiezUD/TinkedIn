6951439fc68bac5c8574072fe35c97b0d80beb97
aitordiezUD <aitor.diez@opendeusto.es>
2023-11-17 11:12:08 +0100

    Arreglo

--- TinkedIn/src/module-info.java
+++ TinkedIn/src/module-info.java
@@ -1,11 +1,9 @@
-/**
- * 
- */
-/**
- * @author aitor
- *
- */
 module TinkedIn {
+	exports pruebas;
+	exports ventanas;
+	exports hilos;
+	exports clases;
+
 	requires java.desktop;
 	requires junit;
 }
